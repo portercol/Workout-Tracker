0b684a6d5c8f6e0bf6408c0e1a7ef17c028bb950
Collin Porter <colls.potta@gmail.com>
2020-11-14 14:59:03 -0700

    removed Dev folder, updated server.js with comments

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(express.json());
 // Serve static content for the app
 app.use(express.static("public"));
 
-// // routes
-// app.use(require("./public/api.js"));
+// routes
+app.use(require("./routes/html.js"));
 
 // Create server listener on PORT #
 app.listen(PORT, () => {
