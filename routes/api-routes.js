8db640b10ed60536b0a8ec36b37e308ed518e9c6
Collin Porter <colls.potta@gmail.com>
2020-11-16 13:15:07 -0700

    added PUT route to update a current workout, using findOneAndUpdate, grabbing the unique ID, if it matches, push it to the existing array of workouts and return the res as json

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
 
     //get all workouts
     app.get("/api/workouts", (req, res) => {
-        console.log(res);
+        // console.log(res);
         db.Workout.find({}).then(dbWorkout => {
             dbWorkout.forEach(workout => {
                 var total = 0;
@@ -19,5 +19,20 @@ module.exports = function (app) {
         });
     });
 
+    // continue exercise
+    app.put("/api/workouts/:id", (req, res) => {
 
+        // Find workout using ID and update it
+        db.Workout.findOneAndUpdate(
+            { _id: req.params.id },
+            {
+                $inc: { totalDuration: req.body.duration },
+                $push: { exercises: req.body }
+            },
+            { new: true }).then(dbWorkout => {
+                res.json(dbWorkout);
+            }).catch(err => {
+                res.json(err);
+            });
+    });
 };
