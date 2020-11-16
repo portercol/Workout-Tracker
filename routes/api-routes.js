fe3717f4878751e5f135e6735cfc50368b86d27a
Collin Porter <colls.potta@gmail.com>
2020-11-16 13:05:01 -0700

    used db.Workout to find all workouts, called the function dbWorkout to use for promise of finding each workout in the array of workouts, if the duration of the event is 0 then equal that to the total duration of the workout and lastly returned the data in json form

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,6 +5,18 @@ module.exports = function (app) {
     //get all workouts
     app.get("/api/workouts", (req, res) => {
         console.log(res);
+        db.Workout.find({}).then(dbWorkout => {
+            dbWorkout.forEach(workout => {
+                var total = 0;
+                workout.exercise.forEach(e => {
+                    total += e.duration;
+                });
+                workout.totalDuration = total;
+            });
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
     });
 
 
